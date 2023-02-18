b8b1429bcab38c847294b5b4e0e098a1878b3e18
unknown <ajfoucaultmo@mail.usf.edu>
2023-02-18 08:51:09 -0800

    Section 02: Creating Content with JSX 17. Customizing Elements with Props. 18. Converting HTML to JSX. 19. Applying Styling in JSX. 20. Exercise Solution. 21. Extracting Components. 22. Module Systems Overview. 23. Cheatsheet for JSX.

--- 01-jsx/src/index.js
+++ 01-jsx/src/index.js
@@ -1,6 +1,7 @@
 // 1) Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 // 2) Get a reference to the div with ID root
 const el = document.getElementById('root');
@@ -8,17 +9,5 @@ const el = document.getElementById('root');
 // 3) Tell React to take control of that element
 const root = ReactDOM.createRoot(el);
 
-// 4) Create a component
-function App() {
-  const name = 'Samantha';
-  const age = 23;
-
-  return (
-    <h1>
-      Hi, my name is {name} and my age is {age}.
-    </h1>
-  );
-}
-
-// 5) Show the component on the screen
+// 4) Show the component on the screen
 root.render(<App />);
