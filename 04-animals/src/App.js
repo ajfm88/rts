4c41c397f7ab3b40d0fec3ef8aca63cfa29e54b8
unknown <ajfoucaultmo@mail.usf.edu>
2023-02-22 07:54:50 -0800

    Section 04: State: How to Change Your App 46. Introducing the State System. 47. More on State. 48. Understanding the Re-Rendering Process. 49. Why Array Destructuring? 50. Back to the App. 51. Picking a Random Element.

--- 04-animals/src/App.js
+++ 04-animals/src/App.js
@@ -1,11 +1,22 @@
+import { useState } from 'react';
+
+function getRandomAnimal() {
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
+  const [animals, setAnimals] = useState([]);
+
   const handleClick = () => {
-    console.log('Button was clicked!');
+    setAnimals([...animals, getRandomAnimal()]);
   };
 
   return (
     <div>
       <button onClick={handleClick}>Add Animal</button>
+      <div>{animals}</div>
     </div>
   );
 }
