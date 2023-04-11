6d11c19109f420eb1163f3fad8e9bad8ea871f96
unknown <ajfoucaultmo@mail.usf.edu>
2023-04-11 12:51:40 -0700

    Section 12: Practicing Props and State Design 207. Event Capture and Bubbling. 208. Putting it All Together. 209. Why a Capture Phase Handler? 210. Reminder on the useEffect Function. 211. Reminder on useEffect Cleanup.

--- 12-comps-props/src/components/Dropdown.js
+++ 12-comps-props/src/components/Dropdown.js
@@ -1,9 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { GoChevronDown } from 'react-icons/go';
 import Panel from './Panel';
 
 function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handler = (event) => {
+      console.log(event.target);
+    };
+
+    document.addEventListener('click', handler, true);
+
+    return () => {
+      document.removeEventListener('click', handler);
+    };
+  }, []);
 
   const handleClick = () => {
     setIsOpen(!isOpen);
@@ -19,7 +31,7 @@ function Dropdown({ options, value, onChange }) {
   const renderedOptions = options.map((option) => {
     return (
       <div
-        className="hover:bg-sky-100 rounded cursor-pointer p-1"
+        className='hover:bg-sky-100 rounded cursor-pointer p-1'
         onClick={() => handleOptionClick(option)}
         key={option.value}
       >
@@ -29,15 +41,15 @@ function Dropdown({ options, value, onChange }) {
   });
 
   return (
-    <div className="w-48 relative">
+    <div className='w-48 relative'>
       <Panel
-        className="flex justify-between items-center cursor-pointer"
+        className='flex justify-between items-center cursor-pointer'
         onClick={handleClick}
       >
         {value?.label || 'Select...'}
-        <GoChevronDown className="text-lg" />
+        <GoChevronDown className='text-lg' />
       </Panel>
-      {isOpen && <Panel className="absolute top-full">{renderedOptions}</Panel>}
+      {isOpen && <Panel className='absolute top-full'>{renderedOptions}</Panel>}
     </div>
   );
 }
