b358bda535fb6dcb76e4bbc46e240bf9d0f4dd19
unknown <ajfoucaultmo@mail.usf.edu>
2023-05-07 07:55:20 -0700

    Section 16: Getting Clever with Data Sorting 265. React Fragments. 266. The Big Reveal.

--- 16-data-sorting/src/components/Table.js
+++ 16-data-sorting/src/components/Table.js
@@ -1,7 +1,9 @@
+import { Fragment } from 'react';
+
 function Table({ data, config, keyFn }) {
   const renderedHeaders = config.map((column) => {
     if (column.header) {
-      return column.header();
+      return <Fragment key={column.label}>{column.header()}</Fragment>;
     }
 
     return <th key={column.label}>{column.label}</th>;
@@ -10,23 +12,23 @@ function Table({ data, config, keyFn }) {
   const renderedRows = data.map((rowData) => {
     const renderedCells = config.map((column) => {
       return (
-        <td className="p-2" key={column.label}>
+        <td className='p-2' key={column.label}>
           {column.render(rowData)}
         </td>
       );
     });
 
     return (
-      <tr className="border-b" key={keyFn(rowData)}>
+      <tr className='border-b' key={keyFn(rowData)}>
         {renderedCells}
       </tr>
     );
   });
 
   return (
-    <table className="table-auto border-spacing-2">
+    <table className='table-auto border-spacing-2'>
       <thead>
-        <tr className="border-b-2">{renderedHeaders}</tr>
+        <tr className='border-b-2'>{renderedHeaders}</tr>
       </thead>
       <tbody>{renderedRows}</tbody>
     </table>
