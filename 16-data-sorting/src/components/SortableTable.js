1d6baf1426578873134f32e7afd7cfba4c23d394
unknown <ajfoucaultmo@mail.usf.edu>
2023-05-12 12:38:47 -0700

    Section 16: Getting Clever with Data Sorting 271. Yessssss, It Worked! 272. Determining Icon Set.

--- 16-data-sorting/src/components/SortableTable.js
+++ 16-data-sorting/src/components/SortableTable.js
@@ -4,7 +4,7 @@ import Table from './Table';
 function SortableTable(props) {
   const [sortOrder, setSortOrder] = useState(null);
   const [sortBy, setSortBy] = useState(null);
-  const { config } = props;
+  const { config, data } = props;
 
   const handleClick = (label) => {
     if (sortOrder === null) {
@@ -27,17 +27,54 @@ function SortableTable(props) {
     return {
       ...column,
       header: () => (
-        <th onClick={() => handleClick()}>{column.label} IS SORTABLE</th>
+        <th onClick={() => handleClick(column.label)}>
+          {getIcons(column.label, sortBy, sortOrder)}
+          {column.label}
+        </th>
       ),
     };
   });
 
+  // Only sort data if sortOrder && sortBy are not null
+  // Make a copy of the 'data' prop
+  // Find the correct sortValue function and use it for sorting
+  let sortedData = data;
+  if (sortOrder && sortBy) {
+    const { sortValue } = config.find((column) => column.label === sortBy);
+    sortedData = [...data].sort((a, b) => {
+      const valueA = sortValue(a);
+      const valueB = sortValue(b);
+
+      const reverseOrder = sortOrder === 'asc' ? 1 : -1;
+
+      if (typeof valueA === 'string') {
+        return valueA.localeCompare(valueB) * reverseOrder;
+      } else {
+        return (valueA - valueB) * reverseOrder;
+      }
+    });
+  }
+
   return (
     <div>
       {sortOrder} - {sortBy}
-      <Table {...props} config={updatedConfig} />
+      <Table {...props} data={sortedData} config={updatedConfig} />
     </div>
   );
+}
+
+function getIcons(label, sortBy, sortOrder) {
+  if (label !== sortBy) {
+    return 'Show both icons';
+  }
+
+  if (sortOrder === null) {
+    return 'show both icons';
+  } else if (sortOrder === 'asc') {
+    return 'show up icon';
+  } else if (sortOrder === 'desc') {
+    return 'show down icon';
+  }
 }
 
 export default SortableTable;
