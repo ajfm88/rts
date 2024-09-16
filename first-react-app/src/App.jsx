1f21d18188b11d9e42a3a6f4514a7288649205e7
ajfm88 <ale@ajfm88.com>
2024-09-15 23:10:23 -0700

    Section 63: Working With Props: 620. Introducing Props. 621. Non-String Props. 622. Setting Default Prop Values. 623. Passing Arrays and Objects.

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,12 +1,22 @@
-import './App.css'
-import Chicken from './Chicken'
+import "./App.css";
+import Chicken from "./Chicken";
+import Greeter from "./Greeter";
+import ListPicker from "./ListPicker";
+import Die from "./Die";
 
 function App() {
-  return <>
-    <Chicken/>
-    <Chicken/>
-    <Chicken/>
-  </>
+  return (
+    <>
+      <ListPicker values={[1, 2, 3]} />
+      <ListPicker values={["a", "b", "c"]} />
+      {/* <Greeter />
+      <Greeter person="Ted" from="Colt" />
+      <Greeter person="Rosa" from="Elton" />
+      <Die numSides={20} />
+      <Die />
+      <Die numSides={10} /> */}
+    </>
+  );
 }
 
-export default App
+export default App;
