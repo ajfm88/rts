a507f69abe94fb884f35889c224829df994aa332
ajfm88 <ale@ajfm88.com>
2025-01-29 20:22:29 -0800

    Quiz 5: TypeScript Functions: Optional Parameters, Rest Parameters, and Void Return Types. 28. Functions - Type Guards.

--- practical-typescript/01-typescript-tutorial/src/tutorial.ts
+++ practical-typescript/01-typescript-tutorial/src/tutorial.ts
@@ -279,7 +279,7 @@ console.log(scoreAfterPenalty); // Output: 80
 
 let scoreWithoutPenalty = calculateScore(300);
 console.log(scoreWithoutPenalty); // Output: 300
-*/
+
 // ## Function - Rest Parameter
 
 // In JavaScript, a rest parameter is denoted by three dots (...) before the parameter's name and allows a function to accept any number of arguments.
@@ -316,3 +316,26 @@ function logMessage(message: string): void {
 }
 
 logMessage("Hello, TypeScript!"); // Output: Hello, TypeScript!
+*/
+// ## Functions - Using Union Types as Function Parameters
+
+// ### Challenge
+
+// Your task is to create a function named processInput that accepts a parameter of a union type string | number. The function should behave as follows:
+
+// - If the input is of type number, the function should multiply the number by 2 and log the result to the console.
+// - If the input is of type string, the function should convert the string to uppercase and log the result to the console.
+
+function processInput(input: string | number) {
+  if (typeof input === "number") {
+    console.log(input * 2);
+  } else {
+    console.log(input.toUpperCase());
+  }
+}
+
+processInput(10); // Output: 20
+processInput("Hello"); // Output: HELLO
+
+// In this example, the processInput function takes a parameter input that can be either a string or a number. Inside the function, we use a type guard
+// (typeof input === 'number') to check the type of input at runtime. If input is a number, we double it. If input is a string, we convert it to uppercase.
