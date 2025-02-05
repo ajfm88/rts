da3d43c261022e0cea437a272e4b4769f02c09c3
ajfm88 <ale@ajfm88.com>
2025-02-04 20:48:37 -0800

    30. Excess Property Check.

--- practical-typescript/01-typescript-tutorial/readme.md
+++ practical-typescript/01-typescript-tutorial/readme.md
@@ -1,26 +1,3 @@
-## Gotcha - Excess Property Checks
-
-```ts
-function createStudent(student: { id: number; name: string }) {
-  console.log(`Welcome to the course ${student.name.toUpperCase()}!!!`);
-}
-
-const newStudent = {
-  id: 5,
-  name: "anna",
-  email: "anna@gmail.com",
-};
-
-createStudent(newStudent);
-createStudent({ id: 1, name: "bob", email: "bob@gmail.com" });
-```
-
-TypeScript only performs excess property checks on object literals where they're used, not on references to them.
-
-In TypeScript, when you pass an object literal (like { id: 1, name: 'bob', email: 'bob@gmail.com' }) directly to a function or assign it to a variable with a specified type, TypeScript checks that the object only contains known properties. This is done to catch common errors.
-
-However, when you pass newStudent to createStudent, TypeScript doesn't complain about the email property. This is because newStudent is not an object literal at the point where it's passed to createStudent.
-
 ## Challenge
 
 Your task is to create a function named processData that accepts two parameters:
--- practical-typescript/01-typescript-tutorial/src/tutorial.ts
+++ practical-typescript/01-typescript-tutorial/src/tutorial.ts
@@ -339,7 +339,7 @@ processInput("Hello"); // Output: HELLO
 
 // In this example, the processInput function takes a parameter input that can be either a string or a number. Inside the function, we use a type guard
 // (typeof input === 'number') to check the type of input at runtime. If input is a number, we double it. If input is a string, we convert it to uppercase.
-*/
+
 //## Functions - Using Objects as Function Parameters
 
 function createEmployee({ id }: { id: number }): {
@@ -364,3 +364,26 @@ const newStudent = {
 };
 
 createStudent(newStudent);
+*/
+// ## Gotcha - Excess Property Checks
+
+function createStudent(student: { id: number; name: string }) {
+  console.log(`Welcome to the course ${student.name.toUpperCase()}!!!`);
+}
+
+const newStudent = {
+  id: 5,
+  name: "anna",
+  email: "anna@gmail.com",
+};
+
+createStudent(newStudent);
+createStudent({ id: 1, name: "bob", email: "bob@gmail.com" });
+
+// TypeScript only performs excess property checks on object literals where they're used, not on references to them.
+
+// In TypeScript, when you pass an object literal (like { id: 1, name: 'bob', email: 'bob@gmail.com' }) directly to a function or assign it to a variable with a
+// specified type, TypeScript checks that the object only contains known properties. This is done to catch common errors.
+
+// However, when you pass newStudent to createStudent, TypeScript doesn't complain about the email property. This is because newStudent is not an object literal
+// at the point where it's passed to createStudent.
