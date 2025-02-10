a2485b152b8041d08d96a25718700e2fa8c963e8
ajfm88 <ale@ajfm88.com>
2025-02-09 17:22:40 -0800

    31. Challenge - Functions II. Quiz 6: TypeScript Functions: Union Types, Objects, and Excess Property Checks.

--- practical-typescript/01-typescript-tutorial/readme.md
+++ practical-typescript/01-typescript-tutorial/readme.md
@@ -1,35 +1,3 @@
-## Challenge
-
-Your task is to create a function named processData that accepts two parameters:
-
-- The first parameter, input, should be a union type that can be either a string or a number.
-- The second parameter, config, should be an object with a reverse property of type boolean, by default it "reverse" should be false
-
-The function should behave as follows:
-
-- If input is of type number, the function should return the square of the number.
-- If input is of type string, the function should return the string in uppercase.
-- If the reverse property on the config object is true, and input is a string, the function should return the reversed string in uppercase.
-
-```ts
-function processData(
-  input: string | number,
-  config: { reverse: boolean } = { reverse: false }
-): string | number {
-  if (typeof input === "number") {
-    return input * input;
-  } else {
-    return config.reverse
-      ? input.toUpperCase().split("").reverse().join("")
-      : input.toUpperCase();
-  }
-}
-
-console.log(processData(10)); // Output: 100
-console.log(processData("Hello")); // Output: HELLO
-console.log(processData("Hello", { reverse: true })); // Output: OLLEH
-```
-
 ## Type Alias
 
 A type alias in TypeScript is a new name or shorthand for an existing type, making it easier to reuse complex types. However, it's important to note that it doesn't create a new unique type - it's just an alias.All the same rules apply to the aliased type, including the ability to mark properties as optional or readonly.
--- practical-typescript/01-typescript-tutorial/src/tutorial.ts
+++ practical-typescript/01-typescript-tutorial/src/tutorial.ts
@@ -364,7 +364,7 @@ const newStudent = {
 };
 
 createStudent(newStudent);
-*/
+
 // ## Gotcha - Excess Property Checks
 
 function createStudent(student: { id: number; name: string }) {
@@ -387,3 +387,33 @@ createStudent({ id: 1, name: "bob", email: "bob@gmail.com" });
 
 // However, when you pass newStudent to createStudent, TypeScript doesn't complain about the email property. This is because newStudent is not an object literal
 // at the point where it's passed to createStudent.
+
+## Challenge
+
+Your task is to create a function named processData that accepts two parameters:
+
+- The first parameter, input, should be a union type that can be either a string or a number.
+- The second parameter, config, should be an object with a reverse property of type boolean, by default it "reverse" should be false
+
+The function should behave as follows:
+
+- If input is of type number, the function should return the square of the number.
+- If input is of type string, the function should return the string in uppercase.
+- If the reverse property on the config object is true, and input is a string, the function should return the reversed string in uppercase.
+*/
+function processData(
+  input: string | number,
+  config: { reverse: boolean } = { reverse: false }
+): string | number {
+  if (typeof input === "number") {
+    return input * input;
+  } else {
+    return config.reverse
+      ? input.toUpperCase().split("").reverse().join("")
+      : input.toUpperCase();
+  }
+}
+
+console.log(processData(10)); // Output: 100
+console.log(processData("Hello")); // Output: HELLO
+console.log(processData("Hello", { reverse: true })); // Output: OLLEH
