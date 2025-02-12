d791508c5a984f69ad071ddd685379a09c569053
ajfm88 <ale@ajfm88.com>
2025-02-11 21:26:29 -0800

    32. Type Alias. 33. Type Alias - Additional Info.

--- practical-typescript/01-typescript-tutorial/src/tutorial.ts
+++ practical-typescript/01-typescript-tutorial/src/tutorial.ts
@@ -400,7 +400,7 @@ The function should behave as follows:
 - If input is of type number, the function should return the square of the number.
 - If input is of type string, the function should return the string in uppercase.
 - If the reverse property on the config object is true, and input is a string, the function should return the reversed string in uppercase.
-*/
+
 function processData(
   input: string | number,
   config: { reverse: boolean } = { reverse: false }
@@ -417,3 +417,70 @@ function processData(
 console.log(processData(10)); // Output: 100
 console.log(processData("Hello")); // Output: HELLO
 console.log(processData("Hello", { reverse: true })); // Output: OLLEH
+*/
+// ## Type Alias
+
+// A type alias in TypeScript is a new name or shorthand for an existing type, making it easier to reuse complex types. However, it's important
+// to note that it doesn't create a new unique type - it's just an alias.All the same rules apply to the aliased type, including the ability to
+// mark properties as optional or readonly.
+
+const john: { id: number; name: string; isActive: boolean } = {
+  id: 1,
+  name: "john",
+  isActive: true,
+};
+const susan: { id: number; name: string; isActive: boolean } = {
+  id: 1,
+  name: "susan",
+  isActive: false,
+};
+
+function createUser(user: { id: number; name: string; isActive: boolean }): {
+  id: number;
+  name: string;
+  isActive: boolean;
+} {
+  console.log(`Hello there ${user.name.toUpperCase()} !!!`);
+  return user;
+}
+
+// Same code, but with type alias implemented:
+
+type User = { id: number; name: string; isActive: boolean };
+
+const john: User = {
+  id: 1,
+  name: "john",
+  isActive: true,
+};
+const susan: User = {
+  id: 1,
+  name: "susan",
+  isActive: false,
+};
+
+function createUser(user: User): User {
+  console.log(`Hello there ${user.name.toUpperCase()} !!!`);
+  return user;
+}
+
+// Additional example of type alias
+
+type StringOrNumber = string | number; // Type alias for string | number
+
+let value: StringOrNumber;
+value = "hello"; // This is valid
+value = 123; // This is also valid
+
+type Theme = "light" | "dark"; // Type alias for theme
+
+let theme: Theme;
+theme = "light"; // This is valid
+theme = "dark"; // This is also valid
+
+// Function that accepts the Theme type alias
+function setTheme(t: Theme) {
+  theme = t;
+}
+
+setTheme("dark"); // This will set the theme to 'dark'
