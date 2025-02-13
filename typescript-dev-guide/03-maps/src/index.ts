62bb4cd7682a86d6709442fac45a8a69e102e67f
ajfm88 <ale@ajfm88.com>
2025-02-12 19:59:12 -0800

    69. Adding Markers. 70. Duplicate Code.

--- typescript-dev-guide/03-maps/src/index.ts
+++ typescript-dev-guide/03-maps/src/index.ts
@@ -1,7 +1,10 @@
-/// <reference types="@types/google.maps" />
-// import { User } from './User';
-// import { Company } from './Company';
+import { User } from './User';
+import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
-import { CustomMap } from "./CustomMap";
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap('map');
 
-new CustomMap("map");
+customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
