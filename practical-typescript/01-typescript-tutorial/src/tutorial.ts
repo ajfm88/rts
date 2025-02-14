02c1a8cf22198afaabfc3b1413797b80aebd6da0
ajfm88 <ale@ajfm88.com>
2025-02-13 19:30:41 -0800

    34. Challenge - Type Alias.

--- practical-typescript/01-typescript-tutorial/src/tutorial.ts
+++ practical-typescript/01-typescript-tutorial/src/tutorial.ts
@@ -417,7 +417,7 @@ function processData(
 console.log(processData(10)); // Output: 100
 console.log(processData("Hello")); // Output: HELLO
 console.log(processData("Hello", { reverse: true })); // Output: OLLEH
-*/
+
 // ## Type Alias
 
 // A type alias in TypeScript is a new name or shorthand for an existing type, making it easier to reuse complex types. However, it's important
@@ -484,3 +484,45 @@ function setTheme(t: Theme) {
 }
 
 setTheme("dark"); // This will set the theme to 'dark'
+*/
+
+// ## Challenge
+
+// - Define the Employee type: Create a type Employee with properties id (number), name (string), and department (string).
+
+// - Define the Manager type: Create a type Manager with properties id (number), name (string), and employees (an array of Employee).
+
+// - Create a Union Type: Define a type Staff that is a union of Employee and Manager.
+
+// - Create the printStaffDetails function: This function should accept a parameter of type Staff. Inside the function, use a type guard to
+// check if the 'employees' property exists in the passed object. If it does, print a message indicating that the person is a manager and the
+// number of employees they manage. If it doesn't, print a message indicating that the person is an employee and the department they belong to.
+
+// - Create Employee and Manager objects: Create two Employee objects. One named alice and second named steve. Also create a Manager object
+// named bob who manages alice and steve.
+
+// - Test the function: Call the printStaffDetails function with alice and bob as arguments and verify the output.
+
+type Employee = { id: number; name: string; department: string };
+type Manager = { id: number; name: string; employees: Employee[] };
+
+type Staff = Employee | Manager;
+
+function printStaffDetails(staff: Staff) {
+  if ("employees" in staff) {
+    console.log(
+      `${staff.name} is a manager of ${staff.employees.length} employees.`
+    );
+  } else {
+    console.log(
+      `${staff.name} is an employee in the ${staff.department} department.`
+    );
+  }
+}
+
+const alice: Employee = { id: 1, name: "Alice", department: "Sales" };
+const steve: Employee = { id: 1, name: "Steve", department: "HR" };
+const bob: Manager = { id: 2, name: "Bob", employees: [alice, steve] };
+
+printStaffDetails(alice); // Outputs: Alice is an employee in the Sales department.
+printStaffDetails(bob);
