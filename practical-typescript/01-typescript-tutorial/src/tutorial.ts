58f52bdde1be42a9f49cd48032fb0f35c67e2085
ajfm88 <ale@ajfm88.com>
2025-03-08 21:15:39 -0800

    39. Interface Methods - More Options.

--- practical-typescript/01-typescript-tutorial/src/tutorial.ts
+++ practical-typescript/01-typescript-tutorial/src/tutorial.ts
@@ -586,7 +586,7 @@ const deepWork: Book = {
 
 deepWork.title = "New Title"; // allowed
 // deepWork.isbn = 654321; // not allowed
-*/
+
 // ## Interface - Methods
 interface Book {
   readonly isbn: number;
@@ -610,6 +610,58 @@ const deepWork: Book = {
     return `${this.title} ${message}`;
   },
 };
+deepWork.printAuthor();
+const result = deepWork.printTitle("is an awesome book");
+console.log(result);
+*/
+// ## Interface - Methods (more options)
+
+// It's generally a good practice to match the structure of the interface and the implementing object or class as
+// closely as possible. This makes the code easier to understand and maintain. So, if printAuthor is defined as a
+// method in the Book interface, it would be more consistent to implement it as a method in the deepWork object.
+interface Book {
+  readonly isbn: number;
+  title: string;
+  author: string;
+  genre?: string;
+  // method
+  printAuthor(): void;
+  printTitle(message: string): string;
+  printSomething: (someValue: number) => number;
+}
+
+const deepWork: Book = {
+  isbn: 9781455586691,
+  title: "Deep Work",
+  author: "Cal Newport",
+  genre: "Self-help",
+  printAuthor() {
+    console.log(this.author);
+  },
+  printTitle(message) {
+    return `${this.title} ${message}`;
+  },
+  // first option
+  // printSomething: function (someValue) {
+  //   return someValue;
+  // },
+  // second option
+  printSomething: (someValue) => {
+    // "this" gotcha
+    console.log(deepWork.author);
+    return someValue;
+  },
+  // third option
+  // printSomething(someValue) {
+  //   return someValue;
+  // },
+  // alternative
+  // printAuthor: () => {
+  //   console.log(deepWork.author);
+  // },
+};
+console.log(deepWork.printSomething(34));
+
 deepWork.printAuthor();
 const result = deepWork.printTitle("is an awesome book");
 console.log(result);
