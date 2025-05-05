4ffd55cf17444492cc22490a0883f0eabf250d36
ajfm88 <ale@ajfm88.com>
2025-05-04 17:59:58 -0700

    49. Type Assertion.

--- practical-typescript/01-typescript-tutorial/src/tutorial.ts
+++ practical-typescript/01-typescript-tutorial/src/tutorial.ts
@@ -1032,7 +1032,7 @@ function getServerResponse(): ServerResponse {
     data: ["first item", "second item"],
   };
 }
-*/
+
 // ## Challenge
 
 // - Define an enum named UserRole with members Admin, Manager, and Employee.
@@ -1071,3 +1071,48 @@ const user: User = createUser({
 });
 
 console.log(user);
+*/
+// ## Type Assertion
+
+// Type assertion in TypeScript is a way to tell the compiler what the type of an existing variable is.
+// This is especially useful when you know more about the type of a variable than TypeScript does.
+let someValue: any = "This is a string";
+
+// Using type assertion to treat 'someValue' as a string
+let strLength: number = (someValue as string).length;
+
+type Bird = {
+  name: string;
+};
+
+// Assume we have a JSON string from an API or local file
+let birdString = '{"name": "Eagle"}';
+let dogString = '{"breed": "Poodle"}';
+
+//
+
+// Parse the JSON string into an object
+let birdObject = JSON.parse(birdString);
+let dogObject = JSON.parse(dogString);
+
+// We're sure that the jsonObject is actually a Bird
+let bird = birdObject as Bird;
+let dog = dogObject as Bird;
+
+console.log(bird.name);
+console.log(dog.name);
+
+enum Status {
+  Pending = "pending",
+  Declined = "declined",
+}
+
+type User = {
+  name: string;
+  status: Status;
+};
+// save Status.Pending in the DB as a string
+// retrieve string from the DB
+const statusValue = "pending";
+
+const user: User = { name: "john", status: statusValue as Status };
