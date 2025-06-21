87b97c8ce7016495c56f40a72c362d40d7bd74a4
ajfm88 <ale@ajfm88.com>
2025-06-21 12:55:37 -0700

    54. Modules - JS File.

--- practical-typescript/01-typescript-tutorial/src/tutorial.ts
+++ practical-typescript/01-typescript-tutorial/src/tutorial.ts
@@ -1233,9 +1233,34 @@ export const something = "something";
 ```json
 "module": "ESNext",
 ```;
-*/
+
 // ## Modules - Imports/Exports (including types)
 import newStudent, { sayHello, person, type Student } from "./actions";
+
+sayHello("TypeScript");
+console.log(person);
+console.log(newStudent);
+
+const anotherStudent: Student = {
+  name: "bob",
+  age: 23,
+};
+
+console.log(anotherStudent);
+*/
+// ## Modules - Javascript Files
+
+// When you set "allowJs": true in your tsconfig.json, TypeScript will process JavaScript files and can infer types to a certain extent
+// based on the structure and usage of your JavaScript code.
+
+// However, TypeScript's ability to infer types from JavaScript is not as robust as when working with TypeScript files. For example,
+// it might not be able to infer complex types or types that depend on runtime behavior.
+
+// - create example.js
+// - export someValue, import in tutorial
+
+import newStudent, { sayHello, person, type Student } from "./actions";
+import { someValue } from "./example.js";
 
 sayHello("TypeScript");
 console.log(person);
