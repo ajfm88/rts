546dc479d9948e4b93fbfb7a359627b063b03079
ajfm88 <ale@ajfm88.com>
2025-06-28 11:06:47 -0700

    56. Type Guards - Equality and in.

--- practical-typescript/01-typescript-tutorial/src/tutorial.ts
+++ practical-typescript/01-typescript-tutorial/src/tutorial.ts
@@ -1272,7 +1272,7 @@ const anotherStudent: Student = {
 };
 
 console.log(anotherStudent);
-*/
+
 // ## Type Guarding
 
 // Type guarding is a term in TypeScript that refers to the ability to narrow down the type of an object within a certain scope.
@@ -1309,3 +1309,28 @@ function checkValue(value: ValueType) {
 }
 
 checkValue(value);
+*/
+// ## Challenge - Equality Narrowing
+
+// In TypeScript, equality narrowing is a form of type narrowing that occurs when you use equality checks like === or !== in your code
+
+// - Define a function named makeSound that takes one parameter animal of type Animal.
+// - Inside the function, use an if statement to check if animal.type is 'dog'.
+// - If animal.type is 'dog', TypeScript knows that animal is a Dog in this block. In this case, call the bark method of animal.
+// - If animal.type is not 'dog', TypeScript knows that animal is a Cat in the else block. In this case, call the meow method of animal.
+// - Now you can call the makeSound function with an Animal as the argument. The function will call the appropriate method (bark or meow)
+// depending on the type of the animal.
+
+type Dog = { type: "dog"; name: string; bark: () => void };
+type Cat = { type: "cat"; name: string; meow: () => void };
+type Animal = Dog | Cat;
+
+function makeSound(animal: Animal) {
+  if (animal.type === "dog") {
+    // TypeScript knows that `animal` is a Dog in this block
+    animal.bark();
+  } else {
+    // TypeScript knows that `animal` is a Cat in this block
+    animal.meow();
+  }
+}
