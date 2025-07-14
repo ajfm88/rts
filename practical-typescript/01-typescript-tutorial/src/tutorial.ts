572087ec2e0da35b59788decc45e2bb6d65e760a
ajfm88 <ale@ajfm88.com>
2025-07-14 09:54:08 -0700

    59. Type Guards - Type Predicate.

--- practical-typescript/01-typescript-tutorial/src/tutorial.ts
+++ practical-typescript/01-typescript-tutorial/src/tutorial.ts
@@ -1387,7 +1387,7 @@ function printLength(str: string | null | undefined) {
 printLength("Hello"); // Outputs: 5
 printLength(null); // Outputs: No string provided
 printLength(undefined); // Outputs: No string provided
-*/
+
 // ## Challenge - "instanceof" type guard
 
 // The instanceof type guard is a way in TypeScript to check the specific class or constructor function of an object at runtime. It returns true
@@ -1425,3 +1425,50 @@ const year = checkInput(new Date());
 const random = checkInput("2020-05-05");
 console.log(year);
 console.log(random);
+*/
+// ## Challenge - Type Predicate
+
+// A type predicate is a function whose return type is a special kind of type that can be used to narrow down types within conditional blocks.
+
+// - Define the Person and Student types. Student should have a study method and Person should have a login method.
+// - Create a function named isStudent that takes a parameter person of type Person.
+// - In the function signature, specify a return type that is a type predicate: person is Student.
+// - In the function body, use a type assertion to treat person as a Student, and check if the study - method is not undefined.
+// This will return true if person is a Student, and false otherwise.
+// - Use the isStudent function in an if statement with person as the argument.
+// - In the if block (where isStudent(person) is true), call the study method on person. TypeScript knows that person is a Student
+// in this block, so this is safe.
+// - In the else block (where isStudent(person) is false), call the login method on person. This is safe because if person is not a
+// Student, it must be a Person, and all Person objects have a login method.
+
+type Student = {
+  name: string;
+  study: () => void;
+};
+
+type User = {
+  name: string;
+  login: () => void;
+};
+
+type Person = Student | User;
+
+const randomPerson = (): Person => {
+  return Math.random() > 0.5
+    ? { name: "john", study: () => console.log("Studying") }
+    : { name: "mary", login: () => console.log("Logging in") };
+};
+
+const person = randomPerson();
+function isStudent(person: Person): person is Student {
+  // return 'study' in person;
+  return (person as Student).study !== undefined;
+}
+
+// Usage
+
+if (isStudent(person)) {
+  person.study(); // This is safe because TypeScript knows that 'person' is a Student.
+} else {
+  person.login();
+}
