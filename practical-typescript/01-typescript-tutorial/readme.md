c67ff97aa9dae17bb2699f7c6c61489dc3271185
ajfm88 <ale@ajfm88.com>
2025-08-24 08:26:16 -0700

    64. Generics - Promise Example.

--- practical-typescript/01-typescript-tutorial/readme.md
+++ practical-typescript/01-typescript-tutorial/readme.md
@@ -1,15 +1,3 @@
-## Generics - Promise Example
-
-```ts
-// A Promise in JavaScript (and thus TypeScript) is an object representing the eventual completion or failure of an asynchronous operation.
-
-async function someFunc(): Promise<string> {
-  return "Hello World";
-}
-
-const result = someFunc();
-```
-
 ## Generics - Generate Array
 
 ```ts
--- practical-typescript/01-typescript-tutorial/src/tutorial.ts
+++ practical-typescript/01-typescript-tutorial/src/tutorial.ts
@@ -1604,3 +1604,13 @@ const genericString: GenericInterface<string> = {
     return this.value;
   },
 };
+
+// ## Generics - Promise Example
+// A Promise in JavaScript (and thus TypeScript) is an object representing the eventual completion
+// or failure of an asynchronous operation.
+
+async function someFunc(): Promise<string> {
+  return "Hello World";
+}
+
+const result = someFunc();
