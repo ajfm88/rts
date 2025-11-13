d7a3c66a009d37b1e504099362de409b872418ff
ajfm88 <ale@ajfm88.com>
2025-11-13 15:08:08 -0800

    70. Fetch Data - Basics.

--- practical-typescript/01-typescript-tutorial/src/tutorial.ts
+++ practical-typescript/01-typescript-tutorial/src/tutorial.ts
@@ -1720,7 +1720,7 @@ function printName<T extends { name: string }>(input: T): void {
 
 printName(student);
 printName(product);
-*/
+
 // ## Generics - Default Value
 interface StoreData<T = any> {
   data: T[];
@@ -1755,3 +1755,38 @@ export interface AxiosResponse<T = any, D = any> {
   config: InternalAxiosRequestConfig<D>;
   request?: any;
 }
+*/
+// ## Fetch Data
+
+// - typically axios and React Query (or both 🚀🚀🚀)
+// - we won't set any state values
+const url = "https://www.course-api.com/react-tours-project";
+
+async function fetchData(url: string) {
+  try {
+    const response = await fetch(url);
+
+    // Check if the request was successful.
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    const errMsg =
+      error instanceof Error ? error.message : "there was an error...";
+    console.error(errMsg);
+    // throw error;
+    return [];
+  }
+}
+
+const tours = await fetchData(url);
+tours.map((tour: any) => {
+  console.log(tour.name);
+});
+
+// return empty array
+// throw error in catch block
+// we are not setting state values in this function
