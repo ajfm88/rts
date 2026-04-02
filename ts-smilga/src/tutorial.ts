64d4597c41f0ce1d129749c3ece3bfa08a3503dc
ａｊｆｍ８８ <ale@ajfm88.com>
2026-04-02 13:08:22 -0700

    04. Type Interface. —   First Challenge.

--- ts-smilga/src/tutorial.ts
+++ ts-smilga/src/tutorial.ts
@@ -1,14 +1,16 @@
-let awesomeName: string = "shakeAndBake";
-awesomeName = "some string";
-awesomeName = awesomeName.toUpperCase();
-console.log(awesomeName);
+// ## Challenge
 
-// awesomeName = 20;
+// - Create a variable of type string and try to invoke a string method on it.
+let greeting: string = "Hello world!";
+let uppercaseGreeting = greeting.toUpperCase();
+console.log(uppercaseGreeting);
+// - Create a variable of type number and try to perform a mathematical operation on it.
+let integer: number = 45;
+let sum = integer + 45;
+console.log(sum);
+// - Create a variable of type boolean and try to perform a logical operation on it.
+let isAwake: boolean = true;
+if (isAwake) console.log("he is awake");
+// - Try to assign a value of a different type to each of these variables and observe the TypeScript compiler's response.
 
-let amount: number = 20;
-amount = 12 - 1;
-// amount = "pants";
-
-let isAwesome: boolean = true;
-isAwesome = false;
-isAwesome = "shakeAndBake";
+// - You can use type annotation or inference
