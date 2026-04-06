3ba4bd1f9c479240b6ea4ffbcaa42c84475ffbb8
ａｊｆｍ８８ <ale@ajfm88.com>
2026-04-06 13:52:21 -0700

    07. Object Fundamentals. —   Challenge - Objects.

--- ts-smilga/src/tutorial.ts
+++ ts-smilga/src/tutorial.ts
@@ -1,35 +1,43 @@
 /*
-## Arrays - Fundamentals
+## Objects - Fundamentals
 
-In TypeScript, arrays are used to store multiple values in a single variable. 
-You can define the type of elements that an array can hold using type annotations.
+In TypeScript, an object is a collection of key-value pairs with specified types for each key, 
+providing static type checking for properties.
 */
-let prices: number[] = [100, 75, 42];
-// prices.push('hello')
+let car: { brand: string; year: number } = { brand: "toyota", year: 2020 };
 
-let fruit: string[] = ["apple", "orange"];
+car.brand = "ford";
+// car.color = 'blue'
 
-// let randomValues: [] = ["hello"];
-let emptyValues: number[] = [];
+let car1: { brand: string; year: number } = { brand: "audi", year: 2022 };
 
-let names = ["peter", "susan", 1];
-let array: (string | boolean)[] = ["apple", true, "orange", false];
+let book = { title: "book", cost: 20 };
+let pen = { title: "pen", cost: 10 };
+let notebook = { title: "notebook" };
+
+let items: { readonly title: string; cost?: number }[] = [book, pen, notebook];
+// items[0].title = "new book"; // Error: Cannot assign to 'title' because it is a read-only property
 
 /*
 ## Challenge
-
-- Create an array temperatures of type number[] and assign it some values. Then, try to add a string value to it.
-- Create an array colors of type string[] and assign it some values. Then, try to add a boolean value to it.
-- Create an array mixedArray of type (number | string)[] and assign it some values. Then, try to add a boolean value to it.
+- Create an object bike of type { brand: string, year: number } and assign it some values. 
+Then, try to assign a string to the year property.
+- Create an object laptop of type { brand: string, year: number } and try to assign an object 
+with missing year property to it.
+- Create an array products of type { title: string, price?: number }[] and assign it some values. 
+Then, try to add an object with a price property of type string to it.
 */
 
-// 1. Temperatures
-let temperatures: number[] = [45, 65, 75, 21];
-// temperatures.push("hot");
+// 1. Bike
+let bike: { brand: string; year: number } = { brand: "Yamaha", year: 2010 };
+// bike.year = 'a string' // This will result in a TypeScript error
 
-// 2. Colors
-let colors: string[] = ["red", "blue", "purple"];
-// colors.push(true)
+// 2. Laptop
+let laptop: { brand: string; year: number } = { brand: "Dell", year: 2020 };
+// let laptop2: { brand: string; year: number } = { brand: "HP" }; // This will result in a TypeScript error
 
-// 3. Mixed Array
-let mixedArray: (number | string)[] = [42, "Alejandro"];
+// 3. Products
+let product1 = { title: "Shirt", price: 20 };
+let product2 = { title: "Pants" };
+let products: { title: string; price?: number }[] = [product1, product2];
+// products.push({ title: "socks", price: "expensive" }); // This will result in a TypeScript error
