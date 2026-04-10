098c0ae871633f1bc2d44e5704f4f42a51a92b40
ａｊｆｍ８８ <ale@ajfm88.com>
2026-04-10 11:24:44 -0700

    11. Type Alias. —   Type Alias - Additional Info. —   Challenge - Type Alias.

--- ts-smilga/src/tasks.ts
+++ ts-smilga/src/tasks.ts
@@ -1,67 +1,100 @@
-const taskForm = document.querySelector<HTMLFormElement>('.form');
-const formInput = document.querySelector<HTMLInputElement>('.form-input');
+/*
+## Type Alias
 
-const taskListElement = document.querySelector<HTMLUListElement>('.list');
-
-type Task = {
-  description: string;
-  isCompleted: boolean;
+A type alias in TypeScript is a new name or shorthand for an existing type, making it easier to reuse complex types. 
+However, it's important to note that it doesn't create a new unique type - it's just an alias.All the same rules apply 
+to the aliased type, including the ability to mark properties as optional or readonly.
+*/
+/*
+With no Type Alias
+*/
+const jack: { id: number; name: string; isActive: boolean } = {
+  id: 1,
+  name: "jack",
+  isActive: true,
+};
+const sally: { id: number; name: string; isActive: boolean } = {
+  id: 1,
+  name: "sally",
+  isActive: false,
 };
 
-const tasks: Task[] = loadTasks();
+function createUserNoTypeAlias(user: { id: number; name: string; isActive: boolean }): {
+  id: number;
+  name: string;
+  isActive: boolean;
+} {
+  console.log(`Hello there ${user.name.toUpperCase()} !!!`);
 
-tasks.forEach(renderTask);
+  return user;
+}
+/*
+With Type Alias
+*/
+type User = { id: number; name: string; isActive: boolean };
 
-function loadTasks(): Task[] {
-  const storedTasks = localStorage.getItem('tasks');
-  return storedTasks ? JSON.parse(storedTasks) : [];
+const john: User = {
+  id: 1,
+  name: "john",
+  isActive: true,
+};
+const susan: User = {
+  id: 1,
+  name: "susan",
+  isActive: false,
+};
+
+function createUser(user: User): User {
+  console.log(`Hello there ${user.name.toUpperCase()} !!!`);
+  return user;
+}
+/*
+Type Alias - Additional info
+*/
+type StringOrNumber = string | number; // Type alias for string | number
+
+let value: StringOrNumber;
+value = "hello"; // This is valid
+value = 123; // This is also valid
+
+type Theme = "light" | "dark"; // Type alias for theme
+
+let theme: Theme;
+theme = "light"; // This is valid
+theme = "dark"; // This is also valid
+
+// Function that accepts the Theme type alias
+function setTheme(t: Theme) {
+  theme = t;
 }
 
-taskForm?.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const taskDescription = formInput?.value;
-  if (taskDescription) {
-    const task: Task = {
-      description: taskDescription,
-      isCompleted: false,
-    };
-    // add task to list
-    addTask(task);
-    // render tasks
-    renderTask(task);
-    // update local storage
-    updateStorage();
-    formInput.value = '';
-    return;
+setTheme("dark"); // This will set the theme to 'dark'
+/*
+Type Alias Challenge
+*/
+// - Define the Employee type: Create a type Employee with properties id (number), name (string), and department (string).
+type Employee = { id: number; name: string; department: string };
+// - Define the Manager type: Create a type Manager with properties id (number), name (string), and employees (an array of Employee).
+type Manager = { id: number; name: string; employees: Employee[] };
+// - Create a Union Type: Define a type Staff that is a union of Employee and Manager.
+type Staff = Employee | Manager;
+// - Create the printStaffDetails function: This function should accept a parameter of type Staff.
+function printStaffDetails(staff: Staff) {
+  //  Inside the function, use a type guard to check if the 'employees' property exists in the passed object.
+  if ("employees" in staff) {
+    //  If it does, print a message indicating that the person is a manager and the number of employees they manage.
+    console.log(`${staff.name} is a manager of ${staff.employees.length} employees.`);
+  } else {
+    //  If it doesn't, print a message indicating that the person is an employee and the department they belong to.
+    console.log(`${staff.name} is an employee in the ${staff.department} department.`);
   }
-  alert('Please enter a task description');
-});
-
-function addTask(task: Task): void {
-  tasks.push(task);
-  console.log(tasks);
 }
 
-function renderTask(task: Task): void {
-  const taskElement = document.createElement('li');
-  taskElement.textContent = task.description;
-
-  // checkbox
-  const taskCheckbox = document.createElement('input');
-  taskCheckbox.type = 'checkbox';
-  taskCheckbox.checked = task.isCompleted;
-
-  // toggle checkbox
-
-  taskCheckbox.addEventListener('change', () => {
-    task.isCompleted = !task.isCompleted;
-    updateStorage();
-  });
-
-  taskElement.appendChild(taskCheckbox);
-  taskListElement?.appendChild(taskElement);
-}
-
-function updateStorage(): void {
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-}
+// - Create Employee and Manager objects: Create two Employee objects. One named alice and second named steve.
+const alice: Employee = { id: 1, name: "Alice", department: "Sales" };
+const steve: Employee = { id: 1, name: "Steve", department: "HR" };
+//  Also create a Manager object named bob who manages alice and steve.
+const bob: Manager = { id: 2, name: "Bob", employees: [alice, steve] };
+// - Test the function: Call the printStaffDetails function with alice and bob as arguments and verify the output.
+printStaffDetails(alice); // Outputs: Alice is an employee in the Sales department.
+printStaffDetails(bob);
