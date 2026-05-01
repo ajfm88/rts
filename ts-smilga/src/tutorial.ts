de099d4054e3e73b6aaa7a3f42c5324b928d233e
ａｊｆｍ８８ <ale@ajfm88.com>
2026-04-30 21:15:10 -0700

    29. Generics - Intro. —   Generics - First Function and Interface.

--- ts-smilga/src/tutorial.ts
+++ ts-smilga/src/tutorial.ts
@@ -1,50 +1,50 @@
 /*
-## Challenge - Discriminated Unions and exhaustive check using the never type
+## Generics - Fundamentals
 
-A discriminated union in TypeScript is a type that can be one of several 
-different types, each identified by a unique literal property 
-(the discriminator), allowing for type-safe handling of each possible variant.
+Generics in TypeScript are a way to create reusable code components 
+that work with a variety of types as opposed to a single one.
+
+In other words, generics allow you to write a function or a class 
+that can work with any data type. You can think of generics as a 
+kind of variable for types.
 */
-type IncrementAction = {
-  amount: number;
-  timestamp: number;
-  user: string;
-};
+// In TypeScript, you can declare an array using two syntaxes:
 
-type DecrementAction = {
-  amount: number;
-  timestamp: number;
-  user: string;
-};
+let array1: string[] = ["Apple", "Banana", "Mango"];
+let array2: number[] = [1, 2, 3];
+let array3: boolean[] = [true, false, true];
 
-type Action = IncrementAction | DecrementAction;
+let array4: Array<string> = ["Apple", "Banana", "Mango"];
+let array5: Array<number> = [1, 2, 3];
+let array6: Array<boolean> = [true, false, true];
 
-// - Write a reducer function that takes the current state and an action,
-// and returns the new state. The reducer function should use a switch
-// statement or if-else chain on the type property of the action to
-// handle each action type differently.
-function reducer(state: number, action: Action): number {
-  switch (action.type) {
-    case "increment":
-      return state + action.amount;
-    case "decrement":
-      return state - action.amount;
-    //- In the default case of the switch statement or the final else clause,
-    // perform an exhaustive check by assigning the action to a variable of type never.
-    // If there are any action types that haven't been handled, TypeScript will give a compile error.
-    default:
-      const unexpectedAction: never = action;
-      throw new Error(`Unexpected action: ${unexpectedAction}`);
-  }
+/*
+## Generics - Create Generic Function and Interface
+*/
+function createString(arg: string): string {
+  return arg;
+}
+function createNumber(arg: number): number {
+  return arg;
 }
 
-const newState = reducer(15, {
-  user: "john",
-  type: "increment",
-  amount: 5,
-  timestamp: 123456,
-});
+// Define a generic function
+function genericFunction<T>(arg: T): T {
+  return arg;
+}
 
-//- Implement the logic for each action type in the reducer function.
-// This typically involves returning a new state based on the current
-// state and the properties of the action.
+const someStringValue = genericFunction<string>("Hello World");
+const someNumberValue = genericFunction<number>(2);
+
+// Define a generic interface
+interface GenericInterface<T> {
+  value: T;
+  getValue: () => T;
+}
+
+const genericString: GenericInterface<string> = {
+  value: "Hello World",
+  getValue() {
+    return this.value;
+  },
+};
