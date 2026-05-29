0a2f3ebaa0eaf05e60db8428cbae4d77c9b86d00
ａｊｆｍ８８ <ale@ajfm88.com>
2026-05-29 16:36:18 -0700

    74. Handling Input Elements. 75. OK But Why? 76. Exercise Solution. 77. Running the Search. 78. Reminder on Async:Await.

--- react-grider/src/components/SearchBar.js
+++ react-grider/src/components/SearchBar.js
@@ -1,14 +1,27 @@
+import { useState } from "react";
+
 function SearchBar({ onSubmit }) {
+  // 1. Create a new piece of state.
+  const [term, setTerm] = useState("");
+
+  // 6. This sends the input form text back to the parent component.
   const handleFormSubmit = (event) => {
     event.preventDefault();
+    onSubmit(term);
+  };
 
-    onSubmit("cars");
+  // 2. Create an event handler to watch for the 'onChange' event
+  const handleChamge = (event) => {
+    // 3. When the 'onChange' event fires, get the value from the input
+    setTerm(event.target.value);
+    // 4. Take that value from the input and use it to update your state.
   };
 
   return (
     <div>
       <form onSubmit={handleFormSubmit}>
-        <input />
+        {/* 5. Pass yout sate to the input as the value prop */}
+        <input onChange={handleChamge} value={term} />
       </form>
     </div>
   );
