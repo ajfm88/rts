bca2a6b4ed00fcb349c5c595e187fafb653818c0
ａｊｆｍ８８ <ale@ajfm88.com>
2026-06-09 13:39:29 -0700

    132. Updating a Record. 133. Thinking About Updates. 134. Deleting a Record.

--- react-grider/src/App.js
+++ react-grider/src/App.js
@@ -15,10 +15,14 @@ function App() {
     fetchBooks();
   }, []);
 
-  const editBookById = (id, newTitle) => {
+  const editBookById = async (id, newTitle) => {
+    const respose = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...respose.data };
       }
 
       return book;
@@ -27,7 +31,8 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
